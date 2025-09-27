247f2b1258515369cb8a70caca450d1c6999da24
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-09-26 19:13:08 -0700

    Fixing files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import './index.css'
+import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<p>Testing</p>)
